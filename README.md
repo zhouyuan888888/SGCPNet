97170fbc47af8025c1914af4ae4a3a41afc0c16f
zhouyuan888888 <zhouyuan888888@gmail.com>
2022-03-26 19:43:56 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # The Pytorch  Implementation of Real-time Semantic Segmentation via Spatial-detail Guided Context Propagation
 
+## Dataset Set
+### *Camvid*
+The Camvid dataset contains 701 images and 32 different semantic categories. We follow the previous works, and evaluate our segmentation model using 11 categories. For the details, please refer the papers [1], [2], [3]. The Camvid dataset can be downloaded from [http://mi.eng.cam.ac.uk/research/projects/VideoRec/CamVid/](http://mi.eng.cam.ac.uk/research/projects/VideoRec/CamVid/).     
 
-### (i) Before running the scripts, please run `python src/setup.py build_ext --build-lib=./src/`.
+### *Cityscapes*
+The Cityscapes dataset contains 25 000 road scene images, in which 5000 images are finely annotated and 20 000 images are labelled with coarse annotation. In our experiments, we only adopt the fine-annotated subset. The fine-annotated subset involves 30 semantic categories. But we follow the previous works, and adopt 19 categories in model evaluation.  The Cityscapes dataset can be downloaded from [http://mi.eng.cam.ac.uk/research/projects/VideoRec/CamVid/](https://www.cityscapes-dataset.com/). Accordingly, the toolkits for data pre-processing can be found at [https://github.com/mcordts/cityscapesScripts](https://github.com/mcordts/cityscapesScripts).
 
-### (ii) For the model training, please run the command `python src/train.py --evaluate False` .
+[1] Combining Appearance and Structure from Motion Features for Road Scene Understanding  
+[2] ICNet for real-time semantic segmentation on high-resolution images  
+[3] Efficient dense modules of asymmetric convolution for real-time semantic segmentation
 
-### (iii) We provide the well-trained model in Google Drive for reproducing our results on the widely used semantic segmentation dataset, Cityscapes. Firstly, put the download checkpoint into the folder `./ckpt`. Secondly, please run the command `python src/test.py`, and the predictions for the test images are saved into the folder  `./ckpt/test_result`. At last,  run the command ` zip -r test_result.zip test_result`, and submit the `test_result.zip`  to the official online evaluator (`https://www.cityscapes-dataset.com/submit/`
+
+## Main Environment
+python==3.6.2  
+pytorch==1.1  
+numpy==1.15  
+torchvision==0.3.0  
+pillow==7.1.2  
+cython==0.29.20  
+scipy==1.1.0  
+scikit-learn==0.16.2  
+
+
+
+##Script Running 
+(1). Before running the scripts, please run `python src/setup.py build_ext --build-lib=./src/`.  
+
+(2). For training the segmentation model, please run the command: `python src/train.py --evaluate [False/True]`.   
+
+(3). We saved the well-trained model on the Cityscapes dataset in [*Google Drive*](https://drive.google.com/drive/folders/1VuN_qSXjU3A1vQhT1JJH4PZbdJcHmdO2?usp=sharing) for reproducing our results. Specifically, firstly, put the download checkpoint into the folder `./ckpt`. Secondly, run the command `python src/test.py`. The predictions for the test images are saved into the folder  `./ckpt/test_result`. Thirdly, please  run the command ` zip -r test_result.zip test_result` in bash. Finally, submit the file `test_result.zip`  to the official online evaluator ([https://www.cityscapes-dataset.com/submit/](https://www.cityscapes-dataset.com/submit/)) to get the final performance in the Cityscapes' test set.
 
---
+++ src/test.py
@@ -0,0 +1,135 @@
+import os
+import pdb
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = (os.path.split(curPath)[0])
+sys.path.append(rootPath)
+
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+import torch
+from torch.utils.data import DataLoader
+import cv2
+import torch.nn as nn
+from thop_old import profile
+from models.SGCPNet import SGCPNet
+
+class ToTensor(object):
+    def __call__(self, sample):
+        image, mask= sample['image'], sample["mask"]
+        image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image), "mask":torch.from_numpy(mask)}
+
+class Normalise(object):
+    def __init__(self, scale, mean, std):
+        self.scale = scale
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, sample):
+        image = sample['image']
+        return {'image': (self.scale * image - self.mean) / self.std, 'mask' : sample['mask']}
+
+
+class CityscapesTest(Dataset):
+    def __init__(self, data_file, data_dir, resolution, transform_test=None):
+        with open(data_file, 'rb') as f:
+            datalist = f.readlines()
+        self.datalist = [(k, v) for k, v in map(lambda x: x.decode('utf-8').strip('\n').split(' '), datalist)]
+        self.root_dir = data_dir
+        self.transform_test = transform_test
+        self.resolution = resolution
+
+    def __len__(self):
+        return len(self.datalist)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, self.datalist[idx][0])
+        msk_name = os.path.join(self.root_dir, self.datalist[idx][1])
+        def read_image(x):
+            img_arr = np.array(Image.open(x))
+            if len(img_arr.shape) == 2: # grayscale
+                img_arr = np.tile(img_arr, [3, 1, 1]).transpose(1, 2, 0)
+            return img_arr
+        image = read_image(img_name)
+        mask = np.array(Image.open(msk_name))
+
+        ##---resize for 1024x1024 input images---##
+        image = Image.fromarray(image)
+        mask = Image.fromarray(mask)
+        image=np.array(image.resize(self.resolution))
+        mask = np.array(mask.resize(self.resolution))
+
+        sample = {'image': image, "mask": mask}
+        sample = self.transform_test(sample)
+        sample["file"] = img_name
+        return sample
+
+def main():
+    num_classes = 19
+    resolution = (2048, 1024)
+    normalise_params = [1. / 255,  np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3)),  np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3))]
+    val_list = './data/test_list.txt'
+    val_dir = './data/cityscapes/'
+    composed_test = transforms.Compose([Normalise(*normalise_params), ToTensor()])
+    load_path = "./ckpt/cityscapes.pth.tar"
+    print(load_path)
+
+    flops_1, params_1 = profile(SGCPNet(num_classes=num_classes), input_size=(1, 3, 2048, 1024), device="cpu")
+    flops_2, params_2 = profile(SGCPNet(num_classes=num_classes), input_size=(1, 3, 1536, 768), device="cpu")
+    print("1024 x 2048==> FLOPs:  {:.2f} G, Params:  {:.2f} M".format(flops_1/1e9, params_1/1e6))
+    print("1536 x 768==> FLOPs:  {:.2f} G, Params:  {:.2f} M".format(flops_2 /1e9, params_2/1e6))
+
+    pdb.set_trace()
+    test_set = CityscapesTest(data_file=val_list, data_dir=val_dir, transform_test=composed_test, resolution=resolution)
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=16, pin_memory=True)
+
+    segmenter = nn.DataParallel(SGCPNet(num_classes=num_classes)).cuda()
+    model_dict = torch.load(load_path)["segmenter"]
+    segmenter.load_state_dict(model_dict)
+    segmenter.eval()
+
+    save_dir = "./ckpt/test_result/"
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+    with torch.no_grad():
+        for i, sample in enumerate(test_loader):
+            input = sample['image']
+            input_var = torch.autograd.Variable(input).float().cuda()
+
+            filename = sample['file'][0].split("/")[-1]
+            filename = "_".join(filename.split("_")[:-1])+".png"
+            save_pth = save_dir+filename
+            output = segmenter(input_var)
+            output = cv2.resize(output[0, :num_classes].data.cpu().numpy().transpose(1, 2, 0),
+                 (2048, 1024), interpolation=cv2.INTER_CUBIC).argmax(axis=2).astype(np.uint8)
+
+            output[output == 255] = 0
+            output[output == 18] = 33
+            output[output == 17] = 32
+            output[output == 16] = 31
+            output[output == 15] = 28
+            output[output == 14] = 27
+            output[output == 13] = 26
+            output[output == 12] = 25
+            output[output == 11] = 24
+            output[output == 10] = 23
+            output[output == 9] = 22
+            output[output == 8] = 21
+            output[output == 7] = 20
+            output[output == 6] = 19
+            output[output == 5] = 17
+            output[output == 4] = 13
+            output[output == 3] = 12
+            output[output == 2] = 11
+            output[output == 1] = 8
+            output[output == 0] = 7
+            cv2.imwrite(save_pth, output)
+
+        print("Testing phrase finishes. Please zip the folder 'test_result', and submit to https://www.cityscapes-dataset.com/submit/ for offical online evaluation")
+
+if __name__ == "__main__":
+    main()
